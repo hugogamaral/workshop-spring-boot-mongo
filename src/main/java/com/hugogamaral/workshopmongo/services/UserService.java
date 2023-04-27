1ac5d9e7ee1bb0bc2189cad23be59558df298fff
Hugo Amaral <hugogamaral@gmail.com>
2023-04-26 23:43:07 -0300

    Obtendo um usuario por id

--- src/main/java/com/hugogamaral/workshopmongo/services/UserService.java
+++ src/main/java/com/hugogamaral/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.hugogamaral.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hugogamaral.workshopmongo.domain.User;
 import com.hugogamaral.workshopmongo.repository.UserRepository;
+import com.hugogamaral.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
