84bfcb21a41efb0432b888e82785b877685c1b7a
Hugo Amaral <hugogamaral@gmail.com>
2023-05-05 00:33:10 -0300

    Consulta simples com query methods

--- src/main/java/com/hugogamaral/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/hugogamaral/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.hugogamaral.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.hugogamaral.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	List<Post> findByTitleContaining(String text);	
 }
